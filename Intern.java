9751c490d07ff2b243c32a1c87a2c5ae307b4518
unknown <bejon.loekito@gmail.com>
2023-07-30 22:10:38 +0700

    Menambahkan addProject

--- Intern.java
+++ Intern.java
@@ -9,4 +9,13 @@ public class Intern extends Employee {
         // TODO
         return super.toString();
     }
+
+    @Override
+    public void addProject(Project project) {
+        if (this.getProjects().size() == 1) {
+            System.out.println("Karyawan " + this.getName() + " hanya dapat mengikuti satu proyek.");
+        } else {
+            this.getProjects().add(project);
+        }
+    }
 }
